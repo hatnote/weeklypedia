9f15ec5647596e27fc916232022a0b6074822461
Stephen LaPorte <stephen.laporte@gmail.com>
2014-01-26 05:43:47 +0000

    adjusting templates

--- weeklypedia/web.py
+++ weeklypedia/web.py
@@ -13,15 +13,17 @@ from mail import Mailinglist, KEY
 HISTORY_FILE = 'history.json'
 _CUR_PATH = os.path.dirname(os.path.abspath(__file__))
 
-def send(sendkey, lang='en'):
+def send(sendkey, lang='en', list_id='119b2cc4d'):
     changes_json = fetch_rc(lang=lang)
     ashes_env = ashes.AshesEnv([_CUR_PATH])
     changes_html = ashes_env.render('template.html', changes_json)
     changes_text = ashes_env.render('template.text', changes_json)
     mailinglist = Mailinglist(sendkey + KEY)
-    mailinglist.new_campaign('Wikipedia digest', 
+    subject = 'Weeklypedia, English Edition'
+    mailinglist.new_campaign(subject, 
                              changes_html, 
-                             changes_text)
+                             changes_text,
+                             list_id=list_id)
     mailinglist.send_next_campaign()
     history = load_history()
     if not history.get(lang):
@@ -36,7 +38,8 @@ def fetch_rc(lang='en'):
     history = load_history()
     changes = RecentChanges(lang=lang)
     ret = changes.all()
-    ret['stats']['issue'] = len(history.get(lang, [])) + 1
+    ret['stats']['issue'] = 1
+    # len(history.get(lang, []))
     ret['stats']['date'] = strftime("%b %d, %Y", gmtime())
     return ret
 
@@ -50,6 +53,8 @@ def main_page():
 
 def create_app():
     sendkey = GetParamMiddleware('sendkey')
+    lang = GetParamMiddleware('lang')
+    list_id = GetParamMiddleware('list_id')
     routes = [('/', main_page, render_basic),
               ('/meta', MetaApplication),
               ('/send', send, render_basic),
