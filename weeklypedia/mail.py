78bffd17ba17665410cd9cba9c5b7f583767e22d
Mahmoud Hashemi <mahmoudrhashemi@gmail.com>
2014-02-22 13:35:34 -0800

    misc formatting and whitespace cleanup

--- weeklypedia/mail.py
+++ weeklypedia/mail.py
@@ -1,36 +1,36 @@
+# -*- coding: utf-8 -*-
+
 import os
-from json import load
+import json
 
 import mailchimp
 
 _CUR_PATH = os.path.dirname(os.path.abspath(__file__))
-KEY = load(open(os.path.join(_CUR_PATH, 'secrets.json'))).get('mc')
+KEY = json.load(open(os.path.join(_CUR_PATH, 'secrets.json'))).get('mc')
 
 
 DEFAULT_LIST = 'a5ecbc7404'
 #              'f811608f69'
+
+
 class Mailinglist(object):
     def __init__(self, key):
         self.client = mailchimp.Mailchimp(key)
         self.next_campaign = self.get_next_campaign()
         print self.client.helper.ping()
 
-    def new_campaign(self, 
-                     subject, 
-                     html_content, 
-                     text_content, 
+    def new_campaign(self,
+                     subject,
+                     html_content,
+                     text_content,
                      list_id=DEFAULT_LIST):
-        opts = {
-            'list_id': list_id,
-            'subject': subject,
-            'from_email': 'weeklypedia@hatnote.com',
-            'from_name': 'Weeklypedia Digest',
-            'to_name': 'Weeklypedia Digest'
-        }
-        cont = {
-            'html': html_content,
-            'text': text_content,
-        }
+        opts = {'list_id': list_id,
+                'subject': subject,
+                'from_email': 'weeklypedia@hatnote.com',
+                'from_name': 'Weeklypedia Digest',
+                'to_name': 'Weeklypedia Digest'}
+        cont = {'html': html_content,
+                'text': text_content}
         resp = self.client.campaigns.create(type='regular',
                                             options=opts,
                                             content=cont)
@@ -48,6 +48,7 @@ class Mailinglist(object):
     def get_next_campaign(self):
         campaigns = self.client.campaigns.list()
         return campaigns['data'][0]['id']
+
 
 if __name__ == '__main__':
     mc = Mailinglist(KEY)
